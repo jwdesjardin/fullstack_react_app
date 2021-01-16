fe9cb25525240b3131c7bbbef2f5a2fc557ca587
joey desjardins <jwdesjardin@gmail.com>
2021-01-16 12:24:27 -0800

    delpoy no cors yes process.cwd

--- api/app.js
+++ api/app.js
@@ -3,27 +3,18 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const path = require('path');
 const dotenv = require('dotenv');
 
-// variable to enable global error logging
-
-// create the Express app
-const app = express();
-
 dotenv.config();
-// setup morgan which gives us http request logging
-app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json());
 
-// TODO setup your api routes here
+// connect to database
 (async () => {
 	const db = require('./models/index');
 
 	try {
-		const response = await db.sequelize.authenticate();
+		await db.sequelize.authenticate();
 		console.log('connection successfully established');
 	} catch (err) {
 		console.log('ERROR in establishing connection');
@@ -32,34 +23,33 @@ app.use(express.json());
 	await db.sequelize.sync();
 })();
 
+const app = express();
+
+// setup morgan which gives us http request logging
+app.use(morgan('dev'));
+// app.use(cors());
+app.use(express.json());
+
 const apiRoutes = require('./routes/api');
 
 app.use('/api', apiRoutes);
 
 // changes __dirname to parent directory
-const split = __dirname.split('/');
-split.pop();
-__dirname = '/' + split.join('/');
+// const split = __dirname.split('/');
+// split.pop();
+// __dirname = '/' + split.join('/');
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/client/build')));
+	app.use(express.static(path.join(process.cwd(), '/client/build')));
 
 	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+		res.sendFile(path.resolve(process.cwd(), 'client', 'build', 'index.html'))
 	);
 } else {
 	app.get('/', (req, res) => {
 		res.send('API is running...');
 	});
 }
-
-// setup a friendly greeting for the root route
-app.get('/', (req, res) => {
-	res.json({
-		message:
-			'Welcome to the REST API project! Head to "/api/courses" to check out the course list'
-	});
-});
 
 // send 404 if no other route matched
 app.use((req, res) => {
