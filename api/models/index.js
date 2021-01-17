086b6f8b99e9d14102ac5f7908909f8a86ce0551
joey desjardins <jwdesjardin@gmail.com>
2021-01-16 20:41:25 -0800

    deploy with pg Client

--- api/models/index.js
+++ api/models/index.js
@@ -6,6 +6,25 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const db = {};
 
+const { Client } = require('pg');
+
+const client = new Client({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+	if (err) throw err;
+	for (let row of res.rows) {
+		console.log(JSON.stringify(row));
+	}
+	client.end();
+});
+
 let sequelize;
 
 if (process.env.NODE_ENV === 'production') {
