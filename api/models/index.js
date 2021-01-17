a2023882e4133c401df4ac3ea6aae80eac5ae2cf
joey desjardins <jwdesjardin@gmail.com>
2021-01-16 21:05:31 -0800

    deploy fresh db

--- api/models/index.js
+++ api/models/index.js
@@ -6,53 +6,12 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const db = {};
 
-const { Client } = require('pg');
-
-const client = new Client({
-	connectionString: process.env.DATABASE_URL,
-	ssl: {
-		rejectUnauthorized: false
-	}
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-	if (err) throw err;
-	for (let row of res.rows) {
-		console.log(JSON.stringify(row));
-	}
-	client.end();
-});
-
-let sequelize;
-
-if (process.env.NODE_ENV === 'production') {
-	console.log('prod 1');
-	sequelize = new Sequelize(
-		process.env.DB_DATABASE,
-		process.env.DB_USER,
-		process.env.DB_PASSWORD,
-		{
-			host: process.env.HOST,
-			dialect: 'postgres',
-			native: true,
-			ssl: true
-		}
-	);
-} else {
-	console.log('dev 2');
-	sequelize = new Sequelize(
-		process.env.DB_DATABASE,
-		process.env.DB_USER,
-		process.env.DB_PASSWORD,
-		{
-			host: process.env.HOST,
-			dialect: 'postgres',
-			native: true,
-			ssl: true
-		}
-	);
+if (process.env.DATABASE_URL) {
+	// the application is executed on Heroku ... use the postgres database
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres',
+		protocol: 'postgres'
+	});
 }
 
 fs
